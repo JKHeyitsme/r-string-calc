542dda4b580896fe99aaca30c65ef8ab15fdff3e
JK Heyitsme <heyitsmejk@hotmail.com>
2016-08-02 15:25:15 +1000

    add extra space as requested

--- strcalc.R
+++ strcalc.R
@@ -2,6 +2,7 @@ compute = function(input_string) {
  # Perform simple arithmetic encoded in input string
   # e.g. '1 + 2' -> 3, or '1 - 2' -> -1
   values = unlist(strsplit(input_string, ' '))
+  
   num0 = as.numeric(values[1])
   operator = values[2]
   num1 = as.numeric(values[3])
